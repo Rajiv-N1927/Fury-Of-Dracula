ecddafa02c135fe2e304dabe4947ab2804f556ba
Rajiv Narayan <Rajiv.narayan12@gmail.com>
2016-09-18 16:14:01 +1000

    Made minor changes to the setPlayers function -> changed NOWHERE to UNKNOWN LOCATION. created helper functions that will make life a lot easier when doing the newGameView function. Everything is piecing together nicely.

--- GameView.c
+++ GameView.c
@@ -12,7 +12,7 @@
 typedef struct player {
   PlayerID playerID;  //Establish an ID for the player
   LocationID curPos;  //The current position of the Player
-  LocationID *trail;  //The trail
+  LocationID trail[TRAIL_SIZE];  //The trail
   int health;         //The current health of the player
 } Player;
 struct gameView {
@@ -26,10 +26,10 @@ static Player *setPlayers(Player p[NUM_PLAYERS]) {
   int i;
   for ( i = 0; i < NUM_PLAYERS; i++ ) {
     p[i].playerID = i;
-    p[i].curPos   = NOWHERE;
-    p[i].trail    = NULL;
+    p[i].curPos   = UNKNOWN_LOCATION;
     p[i].health   = (i == PLAYER_DRACULA ? GAME_START_BLOOD_POINTS
                     : GAME_START_HUNTER_LIFE_POINTS);
+    p[i].trail    = {UNKNOWN_LOCATION};
   }
   return p;
 }
@@ -43,6 +43,40 @@ static GameView init() {
   gameView->newMap  = newMap();
 
   return gameView;
+}
+//How this would be used setTrail(currentView, )
+void setTrail(GameView currentView, PlayerID pID, LocationID locID) {
+  int i;
+  for ( i = 0; i < 6; i++ ) {
+    if ( roundNo >= 6 ) {
+      currentView->player[pID].trail[i] = currentView->player[pID].trail[i+1];
+      if ( currentView->player[pID].trail[TRAIL_SIZE-1] == UNKNOWN_LOCATION ) {
+        currentView->player[pID].trail[TRAIL_SIZE-1] = locID;
+      }
+    } else {
+      if ( currentView->player[pID].trail[i] == UNKNOWN_LOCATION ) {
+        currentView->player[pID].trail[i] = locID;
+        break;
+      }
+    }
+  }
+}
+//Converts the player char to ID
+PlayerID charToPlayerID(char p) {
+  PlayerID thePlayer;
+  switch(p) {
+    case 'G': thePlayer = PLAYER_LORD_GODALMING;
+      break;
+    case 'S': thePlayer = PLAYER_DR_SEWARD;
+      break;
+    case 'H': thePlayer = PLAYER_VAN_HELSING;
+      break;
+    case 'M': thePlayer = PLAYER_MINA_HARKER;
+      break;
+    case 'D': thePlayer = PLAYER_DRACULA;
+      break;
+  }
+  return thePlayer;
 }
 //
 // int main( int argc, char* argv[] ) {
@@ -83,11 +117,10 @@ Round getRound(GameView currentView)
 {
   return currentView->roundNo;
 }
-
 // Get the id of current player - ie whose turn is it?
 PlayerID getCurrentPlayer(GameView currentView)
 {
-  return currentView->player[currentView->turnNo--].playerID;
+  return currentView->player[5%currentView->turnNo+1].playerID;
 }
 
 // Get the current score
@@ -114,7 +147,7 @@ LocationID getLocation(GameView currentView, PlayerID player)
 void getHistory(GameView currentView, PlayerID player,
                             LocationID trail[TRAIL_SIZE])
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    trail = currentView->player[player].trail;
 }
 
 //// Functions that query the map to find information about connectivity
