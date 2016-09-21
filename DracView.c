c29e1f0cb6672938ed09954331c55b80123ff4b9
MItaliano <michael.italiano39@gmail.com>
2016-09-21 23:26:01 +1000

    Update DracView.c

--- DracView.c
+++ DracView.c
@@ -20,6 +20,81 @@ DracView newDracView(char *pastPlays, PlayerMessage messages[])
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
     DracView dracView = malloc(sizeof(struct dracView));
     dracView->hello = 42;
+    
+    // THE BELOW WAS TAKEN FROM GAMEVIEW AND HAS TO BE TWEAKED FOR DRACVIEW 
+     /* I guess these loops can just be put in dracview
+
+        if (currentPlayer == PLAYER_DRACULA) { 
+
+                while (actionLoop < 2) { // placement phase i.e. if trap or vamp was placed
+
+                    if (pastPlays[actionIndex] == 'V') { // vamp placed
+
+                       // updateVampire(turnAbbrevLocation, roundNo, actionInt)
+                            // need to place an immature vampire here 
+                            // should be something laong the lines of...
+                            // updateVampire(turnAbbrevLocation, roundNo, actionVariable)
+                            // action variable can be defined to be the placing or vanquishing or maturing of a vampire
+                            // we could DEFINE VAMPIRE_PLACE, VAMPIRE_VANQUISH & VAMPIRE_MATURE to determine that 
+
+                    }
+
+
+                    if (pastPlays[actionIndex] == 'T') { // trap placed
+
+                        // updateTraps(turnAbbrevLocation, roundNo, actionInt) 
+                                                        // something like... 
+                            // updateTraps(turnAbbrevLocation, roundNo, actionInt) // actionInt will tell if added or disarmed
+                            // in this case an actionInt of 1 could mean adding a trap here
+                            // we could DEFINE TRAP_ADD & TRAP_DISARM & TRAP_VANISH to determine that 
+
+                    }
+
+                    actionIndex++;
+                    actionLoop++;
+ 
+                }
+
+                actionLoop = 0; // resetting for his 'action' phase
+
+               while (actionLoop < 2 && gameStatus == GAME_IN_PROGRESS) { // loops through actions and accounts for effects (only 4 actions per string)
+
+                    if (pastPlays[actionIndex] == 'V') { // vamp matured
+
+                        // updateVampire(turnAbbrevLocation, roundNo, actionInt)
+                            // action int should signify the maturing of a vampire 
+                            // input location here should not matter 
+                            // based on the round No input, the function should search for the respective vampire and remove it from the array 
+                        gameView->score -= SCORE_LOSS_VAMPIRE_MATURES; 
+
+
+                    }
+
+
+                    if (pastPlays[actionIndex] == 'T') { // trap vanished
+
+                        // updateTraps(turnAbbrevLocation, roundNo, actionInt)
+                            // input location here should not matter  
+                            // based on the action int, the function should knwo to merely search its array and remove any outdated traps
+
+                    }
+
+                    actionIndex++;
+                    actionLoop++;
+
+
+                    if (gameView->score <= 0) { 
+
+                        gameStatus = DRACULA_WIN; 
+
+                    }
+ 
+              }  
+
+
+
+        } */ 
+        
     return dracView;
 }
      
